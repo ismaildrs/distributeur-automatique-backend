e37f47562de78af6d6ef7f82571d36621aa1aca9
ismaildrs <drissiisismail@gmail.com>
2025-06-02 22:45:17 +0100

    added required dependencies

---
+++ src/main/java/io/zenika/ismaildrissi/distributeur_automatique_backend/DistributeurAutomatiqueBackendApplication.java
@@ -0,0 +1,13 @@
+package io.zenika.ismaildrissi.distributeur_automatique_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributeurAutomatiqueBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributeurAutomatiqueBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/zenika/ismaildrissi/distributeur_automatique_backend/DistributeurAutomatiqueBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zenika.ismaildrissi.distributeur_automatique_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributeurAutomatiqueBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
