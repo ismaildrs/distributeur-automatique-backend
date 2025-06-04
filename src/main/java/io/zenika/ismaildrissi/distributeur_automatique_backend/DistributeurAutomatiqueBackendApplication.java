ff481f432194b2ae10eb92c64c9286f23487856f
ismaildrs <drissiisismail@gmail.com>
2025-06-04 12:59:35 +0100

    established the folder structure

--- src/main/java/io/zenika/ismaildrissi/distributeur_automatique_backend/DistributeurAutomatiqueBackendApplication.java
+++ src/main/java/io/zenika/ismaildrissi/distributeur_automatique_backend/DistributeurAutomatiqueBackendApplication.java
@@ -3,9 +3,15 @@ package io.zenika.ismaildrissi.distributeur_automatique_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class DistributeurAutomatiqueBackendApplication {
 
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(DistributeurAutomatiqueBackendApplication.class, args);
     }
